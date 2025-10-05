6e64cb61ae047abb158a0409feccb0fcbf430220
Dinuka Rathnayake <it22140616@my.sliit.lk>
2025-10-05 14:30:00 +0530

    Create UserInfoResponse DTO for user details

--- backend/src/main/java/com/swms/dto/UserInfoResponse.java
+++ backend/src/main/java/com/swms/dto/UserInfoResponse.java
@@ -1,5 +1,17 @@
 package com.swms.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserInfoResponse {
-    
+    private String userId;
+    private String username;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String address;
 }
