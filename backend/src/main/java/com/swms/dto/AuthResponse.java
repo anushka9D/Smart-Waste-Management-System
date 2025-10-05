81e73510f06fe8219b9c728fe8c0c627ad058ea8
Dinuka Rathnayake <it22140616@my.sliit.lk>
2025-10-05 13:00:00 +0530

    Create AuthResponse DTO for authentication responses

--- backend/src/main/java/com/swms/dto/AuthResponse.java
+++ backend/src/main/java/com/swms/dto/AuthResponse.java
@@ -1,5 +1,17 @@
 package com.swms.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthResponse {
-    
+    private String token;
+    private String userId;
+    private String username;
+    private String name;
+    private String email;
+    private String message;
 }
