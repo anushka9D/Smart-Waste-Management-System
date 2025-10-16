9861ad4a000a4ccc79f42cf8ec85c4fb139d3754
Anushka Dilshan <anushkadilshan990@gmail.com>
2025-10-16 11:21:20 +0530

    create the alert, binsensor and smartbin models and update the GPS location model

--- backend/src/main/java/com/swms/model/GPSLocation.java
+++ backend/src/main/java/com/swms/model/GPSLocation.java
@@ -1,34 +1,31 @@
 package com.swms.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class GPSLocation {
-    private double latitude;
-    private double longitude;
-    
-    // Default constructor
-    public GPSLocation() {
-    }
-    
-    // Constructor with all fields
-    public GPSLocation(double latitude, double longitude) {
-        this.latitude = latitude;
+    private Double latitude;
+    private Double longitude;
+
+    public void setLongitude(Double longitude) {
         this.longitude = longitude;
     }
-    
-    // Getters
-    public double getLatitude() {
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLatitude() {
         return latitude;
     }
-    
-    public double getLongitude() {
+
+    public Double getLongitude() {
         return longitude;
-    }
-    
-    // Setters
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-    
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
     }
 }
